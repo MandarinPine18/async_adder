a0502af33aa58f204ae84bc2e557897c1836902b
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-01-01 23:28:44 -0600

    Add files via upload

---
+++ async_adder/scripts/script.py
@@ -0,0 +1,12 @@
+from thumbnailer.tasks import adding_task
+
+
+def run(x=0, y=0):
+    try:
+        task = adding_task.delay(int(x), int(y))
+        print(task.get())
+    except ValueError:
+        err1 = "Invalid arguments to the script. Place at most two numeric values after --script-args separated by a "
+        err2 = "space or no values, the default is 0."
+        errstr = err1 + err2
+        print(errstr)
